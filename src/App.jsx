50b7d68217d1c84e399213ecfa42ff8fcb0ef7d7
Lucas Lara <lucasml@live.com>
2022-09-07 19:54:58 -0300

    styleComponents Files Not implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 
 import GeneralLayout from './layout/GeneralLayout';
+import Favorites from './pages/Favorites';
+import About from './pages/About';
+
 
 function App() {
   return (
@@ -9,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<GeneralLayout/>}>
           <Route path={"/"} element={<Home/>} />
-          <Route path={"/"} element={<Home/>} />
-          <Route path={"/"} element={<Home/>} />
+          <Route path={"/favorites"} element={<Favorites />} />
+          <Route path={"/about"} element={<About/>} />
         </Route>
       </Routes>
     </>
