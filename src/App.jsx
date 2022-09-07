99d6b06fc6be7e2355f725b171c2bc8cb5c74570
Lucas Lara <lucasml@live.com>
2022-09-07 18:47:27 -0300

    Pages, Outlet and Components Templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import {useEffect, useState} from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
 
+import GeneralLayout from './layout/GeneralLayout';
 
 function App() {
-  const [pokemon, setPokemon] = useState({})
-  const [pokemonSearch, setPokemonSearch] = useState('')
-
-  
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-      .then(res => res.json())
-      .then(data => setPokemon(data))
-  }, [])
-
-  console.log(pokemon.stats[0].base_stat)
   return (
-    <div>
-      <label htmlFor='pokemonInput'> Digite seu Pokemon
-        <input type="text" id='pokemonInput' />
-      </label>
-      <p>{pokemon.name}</p>
-      <img src={pokemon.sprites.front_default} alt="" />
-      <p>Habilidades</p>
-      <ul>
-        {pokemon.abilities.map(({ability: {name}}) => <li>{name}</li> )}
-      </ul>
-      <p>Stats</p>
-      <ul>
-        {pokemon.abilities.map(({ability: {name}}) => <li>{name}</li> )}
-      </ul>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<GeneralLayout/>}>
+          <Route path={"/"} element={<Home/>} />
+          <Route path={"/"} element={<Home/>} />
+          <Route path={"/"} element={<Home/>} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
