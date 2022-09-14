a0a5062c29dae8ceda2272ec6925ed4eed626842
Lucas Lara <lucasml@live.com>
2022-09-14 11:12:53 -0300

    Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import { StyledHeader, HeaderImg } from './HeaderStyle'
+
+import logo from '../assets/1.svg'
+
 export default function Header() {
   return (
-    <div>Header</div>
+    <StyledHeader>
+      <HeaderImg src={logo} style={{ height: '80%', background: 'inherit' }} alt='logo' />
+    </StyledHeader>
   )
 }
--- src/Components/HeaderStyle.jsx
+++ src/Components/HeaderStyle.jsx
@@ -1,1 +1,18 @@
 import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 3em;
+  background: hsl(0,100%,80%);
+  color: #ffff;
+  font-size: 2em;
+`
+const HeaderImg = styled.img`
+  height: '80%';
+  background: 'inherit'
+`
+  
+
+export { StyledHeader, HeaderImg }
